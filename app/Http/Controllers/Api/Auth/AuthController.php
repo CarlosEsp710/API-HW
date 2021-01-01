f0ecbd539750f5ff23e7d0f1b829245e308dfd7f
Carlos Espejel <carlosespejel71@gmail.com>
2021-01-01 14:25:00 -0600

    Controller Api/Auth & Api routes from auth

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    private $response = [
+        'message' => null,
+        'data' => null
+    ];
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $data = User::created([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $this->response['message'] = 'success';
+
+        return response()->json($this->response, 200);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request, $user->password)) {
+            return response()->json([
+                'message' => 'Failed',
+                'message' => 'Email or Password is failed'
+            ]);
+        }
+
+        $token = $user->createToken($request->device_name)->plainTextToken;
+        $this->response['message'] = 'success';
+        $this->response['data'] = [
+            'token' => $token
+        ];
+
+        return response()->json($this->response, 200);
+    }
+
+    public function me()
+    {
+        $user = Auth::user();
+
+        $this->response['message'] = 'success';
+        $this->response['data'] = $user;
+
+        return response()->json($this->response, 200);
+    }
+
+    public function logout()
+    {
+        $logout = auth()->user()->currentAccessToken()->delete();
+
+        $this->response['message'] = 'success';
+
+        return response()->json($this->response, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('auth/register', [App\Http\Controllers\Api\Auth\AuthController::class, 'register']);
+Route::post('auth/login', [App\Http\Controllers\Api\Auth\AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('auth/me', [App\Http\Controllers\Api\Auth\AuthController::class, 'me']);
+    Route::post('auth/logout', [App\Http\Controllers\Api\Auth\AuthController::class, 'logout']);
 });
