82795ca58e9fb1f1cbe2c69cf9fb14ea9c6cdc00
Carlos Espejel <carlosespejel71@gmail.com>
2021-01-01 14:43:33 -0600

    Corrigiendo error del login

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -43,7 +43,7 @@ class AuthController extends Controller
 
         $user = User::where('email', $request->email)->first();
 
-        if (!$user || !Hash::check($request, $user->password)) {
+        if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json([
                 'message' => 'Failed',
                 'message' => 'Email or Password is failed'
