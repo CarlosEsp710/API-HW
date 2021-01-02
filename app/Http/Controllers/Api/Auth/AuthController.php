1fe8d4e132c40f1b58c6a4c3ddf7efaf68253aec
Carlos Espejel <carlosespejel71@gmail.com>
2021-01-02 16:01:51 -0600

    Optimización de el controlador

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -7,74 +7,81 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    private $response = [
-        'message' => null,
-        'data' => null
-    ];
-
     public function register(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'email' => 'required',
+            'email' => 'required|email',
             'password' => 'required'
         ]);
 
-        $data = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
+        if ($validator->failed()) {
+            return response()->json(['status_code' => 400, 'message' => 'Bad request']);
+        }
+
+        $user =  new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        return response()->json([
+            'status_code' => 200,
+            'message' => 'User created successfully!'
         ]);
-
-        $this->response['message'] = 'success';
-
-        return response()->json($this->response, 200);
     }
 
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required',
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
             'password' => 'required'
         ]);
 
-        $user = User::where('email', $request->email)->first();
+        if ($validator->failed()) {
+            return response()->json(['status_code' => 400, 'message' => 'Bad request']);
+        }
 
-        if (!$user || !Hash::check($request->password, $user->password)) {
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
             return response()->json([
-                'message' => 'Failed',
-                'message' => 'Email or Password is failed'
+                'state_code' => 500,
+                'message' => 'Unauthorized',
             ]);
         }
 
-        $token = $user->createToken($request->device_name)->plainTextToken;
-        $this->response['message'] = 'success';
-        $this->response['data'] = [
-            'token' => $token
-        ];
+        $user = User::where('email', $request->email)->first();
 
-        return response()->json($this->response, 200);
+        $tokenResult = $user->createToken('authToken')->plainText();
+
+        return response()->json([
+            'status_code' => 200,
+            'token' => $tokenResult
+        ]);
     }
 
-    public function me()
+    public function logout(Request $request)
     {
-        $user = Auth::user();
+        $request->user()->currentAccessToken()->delete();
 
-        $this->response['message'] = 'success';
-        $this->response['data'] = $user;
-
-        return response()->json($this->response, 200);
+        return response()->json([
+            'status_code' => 200,
+            'message' => 'Token deleted'
+        ]);
     }
+    // public function me()
+    // {
+    //     $user = Auth::user();
 
-    public function logout()
-    {
-        $logout = auth()->user()->currentAccessToken()->delete();
+    //     $this->response['message'] = 'success';
+    //     $this->response['data'] = $user;
 
-        $this->response['message'] = 'success';
+    //     return response()->json($this->response, 200);
+    // }
 
-        return response()->json($this->response, 200);
-    }
 }
